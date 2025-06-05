5b5480ac5064fd4eedc5d5d14986b71944f94cab
wadim.rudomann <wadim.rudomann@ilcgroup.com>
2025-06-05 14:34:57 +0200

    vite.config geändert

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
+
+viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(import.meta.dirname, "attached_assets"),
+          dest: "", // Kopiert nach dist/public/attached_assets
+        },
+      ],
+    }),    
+
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
